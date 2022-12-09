7e8d7472f58a751b74c481c3c748ca54300da2cb
talgat <15talgat@mail.ru>
2022-12-09 18:18:18 +0300

    zadacha_2

---
+++ zadacha_2/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+Console.Clear();
+
+int[] FillArrayWithRandomNumbers(int size, int leftRange, int rightRange)
+{
+    Random rand = new Random();
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1); // от минуса 9 до 10 (10 не включается)
+    }
+    return arr;
+}
+
+int SummOddNumbers(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum += arr[i];
+        }
+    }
+    return sum;
+}
+
+Console.WriteLine("введите размер массива; ");
+int size = int.Parse(Console.ReadLine());
+int leftRange = 1;
+int rightRange = 10;
+
+int[] array = new int[size];
+array = FillArrayWithRandomNumbers(size, leftRange, rightRange);
+System.Console.WriteLine('[' + string.Join(",", array) + ']');
+int result = SummOddNumbers(array);
+Console.WriteLine(result);
