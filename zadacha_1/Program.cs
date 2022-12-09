02bab501d7c4dd7a5a1ac2db41c9ad032f3c1d5f
talgat <15talgat@mail.ru>
2022-12-09 18:05:33 +0300

    zadacha_1 done

---
+++ zadacha_1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+//  Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.Clear();
+
+int[] FillArrayWithRandomNumbers(int size, int leftRange, int rightRange)
+{
+    Random rand = new Random();
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1); // от минуса 9 до 10 (10 не включается)
+    }
+    return arr;
+}
+int size = 5;
+int leftRange = 100;
+int rightRange = 999;
+int[] array = new int[size];
+array = FillArrayWithRandomNumbers(size, leftRange, rightRange);
+System.Console.WriteLine('[' + string.Join(",", array) + ']');
+
+int AmountEvenNumbers(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int number = AmountEvenNumbers(array);
+System.Console.WriteLine(number);
+
